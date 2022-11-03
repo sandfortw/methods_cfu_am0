df7c42810b769a3941b0699fd2218d15c249c7eb
Weston Sandfort <sandfortw@gmail.com>
2022-11-03 12:25:19 -0600

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -5,23 +5,33 @@
 # The downcase method is called on the string object "Hello World"
 # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
 # The return value is "hello world"
-"Hello World".downcase
+p "Hello World".downcase
 
 # YOU DO the rest:
 
+#The include method is called on the string object "Hello World"
+#The argument "Hello" is passed; include sees if the argument is included in the string. 
+#Because "Hello" is part of the string "Hello World", the return value is true
+p "Hello World".include?("Hello")
 
-"Hello World".include?("Hello")
+#The end_with method is called on the string object "Hello World".
+#The argument "Hello" is passed; end_with sees if the argument ends with the argument.
+#Because "Hello World" does not end with "Hello", the return value is false.
+p "Hello World".end_with?("Hello")
 
+#The end_with method is called on the string object "Hello World".
+#The argument "rld" is passed; end_with sees if the argument ends with the argument.
+#Because "Hello World" endd with "rld", the return value is true.
+p "Hello World".end_with?("rld")
 
-"Hello World".end_with?("Hello")
+#The even? method is called on the integer object "12"
+#Even checks if an integer is even.
+#Because it is even, the return value is true
+p 12.even?
 
-
-"Hello World".end_with?("rld")
-
-
-12.even?
-
-
-18.next
+#The next method is called on the integer object 18
+#The next method returns the next integer after the object.
+#Because 19 is the next integer, the return value is 19
+p 18.next
 
 
