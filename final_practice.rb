c08f4f7d8fc3bdb53fa447608dcd1f3eaab7945b
Weston Sandfort <sandfortw@gmail.com>
2022-11-03 13:10:43 -0600

    Add final_practice and self_eval

--- final_practice.rb
+++ final_practice.rb
@@ -1,38 +1,68 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
 
+def greet_generic
+    p "Hello buddy!"
+end
+
+greet_generic
+
+
 # What is the return value of your method?
+#   The return value is "Hello buddy!"
 # How many arguments did you pass your method?
+#   I passed my method no arguments.
 
 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
 
+def greet_specific(name)
+    p "Hello #{name}!"
+end
+
+greet_specific("Weston")
 
 # What is the return value of your method?
+#   The return value is "Hello Weston!"
 # How many arguments did you pass your method?
+#   I passed my method one argument.
 # What data type was your argument(s)?
-
+#   My data type was string.
 
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
 
+def square(int)
+p int*int
+end
 
+square(2)
 
 # What is the return value of your method?
+#   4
 # How many arguments did you pass your method?
+#   One.
 # What data type was your argument(s)?
-
+#   Integer.
 
 
 
 #4: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
 
+def greet_person(first, middle, last)
+    p "Hello #{first} #{middle} #{last}, how are you today?"
+end
 
+greet_person("John", "Toe", "Doe")
 
 # What is the return value of your method?
+#   "Hello John Toe Doe, how are you today?"
+
 # How many arguments did you pass your method?
+#   Three.
 # What data type was your argument(s)?
+#   Strings.
 
--- self_eval.md
+++ self_eval.md
@@ -1,5 +1,8 @@
 # Self Evaluation
 
 - How confident do you feel about your use of built-in Ruby methods?
+#Pretty good!#
 - How confident do you feel writing simple methods of your own?
+#Also pretty confidentQ#
 - What questions do you have about methods, and what they are capable of? (ask a question!!!)
+#My one question has to do with VS Code. When I edit some object that appears throughout my code, VS code starts to highlight them all, as if I can edit them all at once. How do I do this?#
